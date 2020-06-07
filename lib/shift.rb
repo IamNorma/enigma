8cb9fafb71bde5dd802fcc5a06153bc9eb74d0bd
Norma Lopez <norma_l304@yahoo.com>
2020-06-06 21:50:40 -0600

    Add #create_five_digit_number with passing test

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,3 +1,6 @@
-class Shift 
+class Shift
 
+  def create_five_digit_number
+    rand(99999).to_s.rjust(5,'0')
+  end
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -1,10 +1,19 @@
 require './test/test_helper'
 require './lib/shift'
+require 'mocha/minitest'
+require 'pry'
 
 class ShiftTest < Minitest::Test
   def test_it_exists
     shift = Shift.new
 
     assert_instance_of Shift, shift
+  end
+
+  def test_it_can_create_random_digits
+    shift = Shift.new
+
+    shift.stubs(:rand).returns("02715")
+    assert_equal "02715", shift.create_five_digit_number
   end
 end
