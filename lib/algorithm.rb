3ccc2f2c94be003f2e98df28d623fc3a85015f09
Norma Lopez <norma_l304@yahoo.com>
2020-06-08 20:17:26 -0600

    Add random_number and date arguments to methods

--- lib/algorithm.rb
+++ lib/algorithm.rb
@@ -8,12 +8,11 @@ class Algorithm
     @character_set = ("a".."z").to_a << " "
   end
 
-  def create_random_number
+  def random_number
     rand(99999).to_s.rjust(5,'0')
   end
 
-  def create_keys
-    random_number = create_random_number
+  def create_keys(random_number)
     {
       A: random_number[0..1].to_i,
       B: random_number[1..2].to_i,
@@ -22,17 +21,17 @@ class Algorithm
     }
   end
 
-  def squared_date
+  def squared_date(date)
     date.to_i ** 2
   end
 
-  def last_four_digits
-    squared = squared_date
+  def last_four_digits(date)
+    squared = squared_date(date)
     squared.to_s[-4..-1].to_i
   end
 
-  def create_offsets
-    four_digits = last_four_digits.to_s
+  def create_offsets(date)
+    four_digits = last_four_digits(date).to_s
     {
       A: four_digits[0].to_i,
       B: four_digits[1].to_i,
@@ -41,9 +40,9 @@ class Algorithm
     }
   end
 
-  def final_shifts
-    keys = create_keys
-    offsets = create_offsets
+  def final_shifts(random_number, date)
+    keys = create_keys(random_number)
+    offsets = create_offsets(date)
     keys.merge(offsets) do |key, key_value, offset_value|
       key_value + offset_value
     end
@@ -59,46 +58,46 @@ class Algorithm
     end.to_h
   end
 
-  def encrypt(message)
+  def encrypt(message, random_number, date)
     message_to_encrypt = split_lowercase_message(message)
     encrypted = []
     message_to_encrypt.map.with_index do |character, index|
       if @character_set.include?(character) == false
         encrypted << character
       elsif index % 4 == 0
-        a_shift = @character_set.rotate(final_shifts[:A])
+        a_shift = @character_set.rotate(final_shifts(random_number, date)[:A])
         encrypted << a_shift[character_set_hash[character]]
       elsif index % 4 == 1
-        b_shift = @character_set.rotate(final_shifts[:B])
+        b_shift = @character_set.rotate(final_shifts(random_number, date)[:B])
         encrypted << b_shift[character_set_hash[character]]
       elsif index % 4 == 2
-        c_shift = @character_set.rotate(final_shifts[:C])
+        c_shift = @character_set.rotate(final_shifts(random_number, date)[:C])
         encrypted << c_shift[character_set_hash[character]]
       elsif index % 4 == 3
-        d_shift = @character_set.rotate(final_shifts[:D])
+        d_shift = @character_set.rotate(final_shifts(random_number, date)[:D])
         encrypted << d_shift[character_set_hash[character]]
       end
     end
     encrypted.join("")
   end
 
-  def decrypt(message)
+  def decrypt(message, random_number, date)
     message_to_decrypt = split_lowercase_message(message)
     decrypted = []
     message_to_decrypt.map.with_index do |character, index|
       if @character_set.include?(character) == false
         decrypted << character
       elsif index % 4 == 0
-        a_shift = @character_set.rotate(-final_shifts[:A])
+        a_shift = @character_set.rotate(-final_shifts(random_number, date)[:A])
         decrypted << a_shift[character_set_hash[character]]
       elsif index % 4 == 1
-        b_shift = @character_set.rotate(-final_shifts[:B])
+        b_shift = @character_set.rotate(-final_shifts(random_number, date)[:B])
         decrypted << b_shift[character_set_hash[character]]
       elsif index % 4 == 2
-        c_shift = @character_set.rotate(-final_shifts[:C])
+        c_shift = @character_set.rotate(-final_shifts(random_number, date)[:C])
         decrypted << c_shift[character_set_hash[character]]
       elsif index % 4 == 3
-        d_shift = @character_set.rotate(-final_shifts[:D])
+        d_shift = @character_set.rotate(-final_shifts(random_number, date)[:D])
         decrypted << d_shift[character_set_hash[character]]
       end
     end
--- test/algorithm_test.rb
+++ test/algorithm_test.rb
@@ -25,51 +25,51 @@ class AlgorithmTest < Minitest::Test
     algorithm = Algorithm.new
 
     algorithm.stubs(:rand).returns("02715")
-    assert_equal "02715", algorithm.create_random_number
+    assert_equal "02715", algorithm.random_number
   end
 
   def test_it_can_create_keys
     algorithm = Algorithm.new
-    number = "02715"
-    expected = {A: 02, B: 27, C: 71, D: 15}
-
-    algorithm.expects(:create_random_number).returns(number)
-    assert_equal expected, algorithm.create_keys
+    # number = "02715"
+     expected = {A: 02, B: 27, C: 71, D: 15}
+    #
+    # algorithm.expects(:create_random_number).returns(number)
+    assert_equal expected, algorithm.create_keys("02715")
   end
 
   def test_squared_date
     algorithm = Algorithm.new
-    algorithm.stubs(:date).returns("040620")
+    # algorithm.stubs(:date).returns("040620")
 
-    assert_equal 1649984400, algorithm.squared_date
+    assert_equal 1672401025, algorithm.squared_date("040895")
   end
 
   def test_it_can_get_last_four_digits
     algorithm = Algorithm.new
-    squared_date = 1649984400
-    algorithm.stubs(:squared_date).returns(squared_date)
+    # squared_date = 1649984400
+    # algorithm.stubs(:squared_date).returns(squared_date)
 
-    assert_equal 4400, algorithm.last_four_digits
+    assert_equal 1025, algorithm.last_four_digits("040895")
   end
 
   def test_it_can_create_offsets
     algorithm = Algorithm.new
-    number = 4400
-    expected = {A: 4, B: 4, C: 0, D: 0}
+    # number = 4400
+    expected = {A: 1, B: 0, C: 2, D: 5}
 
-    algorithm.expects(:last_four_digits).returns(number)
-    assert_equal expected, algorithm.create_offsets
+    # algorithm.expects(:last_four_digits).returns(number)
+    assert_equal expected, algorithm.create_offsets("040895")
   end
 
   def test_it_can_create_final_shifts
     algorithm = Algorithm.new
-    keys = {A: 02, B: 27, C: 71, D: 15}
-    offsets = {A: 4, B: 4, C: 0, D: 0}
-    expected = {A: 6, B: 31, C: 71, D: 15}
+    # keys = {A: 02, B: 27, C: 71, D: 15}
+    # offsets = {A: 4, B: 4, C: 0, D: 0}
+    expected = {:A=>3, :B=>27, :C=>73, :D=>20}
 
-    algorithm.expects(:create_offsets).returns(offsets)
-    algorithm.expects(:create_keys).returns(keys)
-    assert_equal expected, algorithm.final_shifts
+    # algorithm.expects(:create_offsets).returns(offsets)
+    # algorithm.expects(:create_keys).returns(keys)
+    assert_equal expected, algorithm.final_shifts("02715", "040895")
   end
 
   def test_it_can_split_message
@@ -93,19 +93,19 @@ class AlgorithmTest < Minitest::Test
 
   def test_it_can_encrypt_message
     algorithm = Algorithm.new
-    shifts = {A: 6, B: 3, C: 1, D: 5}
-
-    algorithm.stubs(:final_shifts).returns(shifts)
-    assert_equal "nhmqucxtxoe", algorithm.encrypt("Hello World")
-    assert_equal "nhmqu,aauumi!", algorithm.encrypt("Hello, World!")
+    # shifts = {A: 6, B: 3, C: 1, D: 5}
+    #
+    # algorithm.stubs(:final_shifts).returns(shifts)
+    assert_equal "keder ohulw", algorithm.encrypt("Hello World", "02715", "040895")
+    assert_equal "keder,sprrdx!", algorithm.encrypt("Hello, World!", "02715", "040895")
   end
 
   def test_it_can_decrypt_message
     algorithm = Algorithm.new
-    shifts = {A: 6, B: 3, C: 1, D: 5}
-
-    algorithm.stubs(:final_shifts).returns(shifts)
-    assert_equal "hello world", algorithm.decrypt("nhmqucxtxoe")
-    assert_equal "hello, world!", algorithm.decrypt("nhmqu,aauumi!")
+    # shifts = {A: 6, B: 3, C: 1, D: 5}
+    #
+    # algorithm.stubs(:final_shifts).returns(shifts)
+    assert_equal "hello world", algorithm.decrypt("keder ohulw", "02715", "040895")
+    assert_equal "hello, world!", algorithm.decrypt("keder,sprrdx!", "02715", "040895")
   end
 end
