e243840a82c87731f8c1d28941dbb74c5e770e41
Norma Lopez <norma_l304@yahoo.com>
2020-06-08 20:20:38 -0600

    Delete comments

--- test/algorithm_test.rb
+++ test/algorithm_test.rb
@@ -30,45 +30,34 @@ class AlgorithmTest < Minitest::Test
 
   def test_it_can_create_keys
     algorithm = Algorithm.new
-    # number = "02715"
      expected = {A: 02, B: 27, C: 71, D: 15}
-    #
-    # algorithm.expects(:create_random_number).returns(number)
+
     assert_equal expected, algorithm.create_keys("02715")
   end
 
   def test_squared_date
     algorithm = Algorithm.new
-    # algorithm.stubs(:date).returns("040620")
 
     assert_equal 1672401025, algorithm.squared_date("040895")
   end
 
   def test_it_can_get_last_four_digits
     algorithm = Algorithm.new
-    # squared_date = 1649984400
-    # algorithm.stubs(:squared_date).returns(squared_date)
 
     assert_equal 1025, algorithm.last_four_digits("040895")
   end
 
   def test_it_can_create_offsets
     algorithm = Algorithm.new
-    # number = 4400
     expected = {A: 1, B: 0, C: 2, D: 5}
 
-    # algorithm.expects(:last_four_digits).returns(number)
     assert_equal expected, algorithm.create_offsets("040895")
   end
 
   def test_it_can_create_final_shifts
     algorithm = Algorithm.new
-    # keys = {A: 02, B: 27, C: 71, D: 15}
-    # offsets = {A: 4, B: 4, C: 0, D: 0}
     expected = {:A=>3, :B=>27, :C=>73, :D=>20}
 
-    # algorithm.expects(:create_offsets).returns(offsets)
-    # algorithm.expects(:create_keys).returns(keys)
     assert_equal expected, algorithm.final_shifts("02715", "040895")
   end
 
@@ -93,18 +82,14 @@ class AlgorithmTest < Minitest::Test
 
   def test_it_can_encrypt_message
     algorithm = Algorithm.new
-    # shifts = {A: 6, B: 3, C: 1, D: 5}
-    #
-    # algorithm.stubs(:final_shifts).returns(shifts)
+
     assert_equal "keder ohulw", algorithm.encrypt("Hello World", "02715", "040895")
     assert_equal "keder,sprrdx!", algorithm.encrypt("Hello, World!", "02715", "040895")
   end
 
   def test_it_can_decrypt_message
     algorithm = Algorithm.new
-    # shifts = {A: 6, B: 3, C: 1, D: 5}
-    #
-    # algorithm.stubs(:final_shifts).returns(shifts)
+  
     assert_equal "hello world", algorithm.decrypt("keder ohulw", "02715", "040895")
     assert_equal "hello, world!", algorithm.decrypt("keder,sprrdx!", "02715", "040895")
   end
