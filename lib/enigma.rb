d628d43d6e6bda50481a0e73623f4851b5b9f767
Norma Lopez <norma_l304@yahoo.com>
2020-06-08 20:25:40 -0600

    Add #encrypt with passing test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,17 @@
-class Enigma 
+require 'date'
 
+class Enigma
+  attr_reader :algorithm
+
+  def initialize
+    @algorithm = Algorithm.new
+  end
+
+  def encrypt(message, key = algorithm.random_number, date = algorithm.date)
+    {
+     encryption: algorithm.encrypt(message, key, date),
+     key: key,
+     date: date
+    }
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,10 +1,23 @@
 require './test/test_helper'
+require './lib/algorithm'
 require './lib/enigma'
+require 'pry'
 
 class EnigmaTest < Minitest::Test
   def test_it_exists
     enigma = Enigma.new
 
-    assert_instance_of Enigma, enigma 
+    assert_instance_of Enigma, enigma
+  end
+
+  def test_it_can_encrypt
+    enigma = Enigma.new
+    expected = {
+     encryption: "keder ohulw",
+     key: "02715",
+     date: "040895"
+    }
+
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end
 end
